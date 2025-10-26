240266e9347b97201f2917b5b2c66b8394f68bf5
Sole <chawoo9905@gmail.com>
2025-10-27 00:03:41 +0900

    test(InputHandler): validateCarName 메소드 기능 테스트

--- src/test/java/racingcar/InputHandlerTest.java
+++ src/test/java/racingcar/InputHandlerTest.java
@@ -6,4 +6,57 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputHandlerTest {
+    @Test
+    void validateNullName() {
+        assertThatThrownBy(() -> InputHandler.validateCarName(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Car name blank error.");
+    }
+
+    @Test
+    void validateBlankName() {
+        assertThatThrownBy(() -> InputHandler.validateCarName("   "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Car name blank error.");
+    }
+
+    @Test
+    void validateEmptyName() {
+        assertThatThrownBy(() -> InputHandler.validateCarName(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Car name blank error.");
+    }
+
+    @Test
+    void validateNameLength() {
+        assertThatThrownBy(() -> InputHandler.validateCarName("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Car name length error.");
+    }
+
+    @Test
+    void validateAlphabetName() {
+        assertThatThrownBy(() -> InputHandler.validateCarName("abc1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Car name is only permitted alphabet.");
+    }
+
+    @Test
+    void validateCarNameContainsSymbol() {
+        assertThatThrownBy(() -> InputHandler.validateCarName("car!"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Car name is only permitted alphabet.");
+    }
+
+    @Test
+    void validateCarNameNoException() {
+        assertThatCode(() -> InputHandler.validateCarName("pobi"))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> InputHandler.validateCarName("A"))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> InputHandler.validateCarName("WONI"))
+                .doesNotThrowAnyException();
+    }
 }
