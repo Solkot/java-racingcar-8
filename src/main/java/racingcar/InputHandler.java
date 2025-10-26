0efb0d78771dd82d824a8021aba7262a8bc28bf6
Sole <chawoo9905@gmail.com>
2025-10-26 23:44:27 +0900

    feat(InputHandler): validateCarName, 이름 검사 메소드 기능 구현

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,4 +1,32 @@
 package racingcar;
 
 public class InputHandler {
+    private static boolean isEmpty(String name) { // 이름 공백 검사
+        return name == null || name.isBlank();
+    }
+
+    private static boolean isLengthInvalid(String name) { // 이름 길이 검사
+        return name.isEmpty() || name.length() > 5;
+    }
+
+    private static boolean containsNonAlphabet(String name) { // 이름 알파벳 여부 검사
+        for (char c : name.toCharArray()) {
+            if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void validateCarName(String name) {
+        if (isEmpty(name)) {
+            throw new IllegalArgumentException("Car name blank error.");
+        }
+        if (isLengthInvalid(name)) {
+            throw new IllegalArgumentException("Car name length error.");
+        }
+        if (containsNonAlphabet(name)) {
+            throw new IllegalArgumentException("Car name is only permitted alphabet.");
+        }
+    }
 }
---
+++ src/test/java/racingcar/InputHandlerTest.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputHandlerTest {
+}
