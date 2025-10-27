ed4ee0141df70ad789941121c05544ab1d3eaab0
Sole <chawoo9905@gmail.com>
2025-10-27 18:25:20 +0900

    feat(Application): Application main 구현 및 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,5 +7,29 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //시작 출력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요");
+        String count = Console.readLine();
+        OutView.printStartMessage();
+
+        // 입력(참가자, 시도 횟수)
+        List<String> carNames = InputHandler.parseCarNames(names);
+        int tryCount = InputHandler.getTryCount(count);
+
+        // 입력 결과 가공, 각 턴의 결과 출력
+        List<Car> cars = OutputHandler.createCars(carNames);
+
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : cars) {
+                OutputHandler.moveCar(car);
+            }
+            OutView.printRaceResult(cars);
+        }
+
+        // 우승자 출력
+        List<Car> winners = OutputHandler.getWinners(cars);
+        OutView.printWinners(winners);
     }
 }
