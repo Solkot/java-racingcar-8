618482cfdd3902f1792d747b3bd9f53c1fb7e827
Sole <chawoo9905@gmail.com>
2025-10-27 18:44:36 +0900

    refactor(InputHandlerTest): @DisplayName을 통한 테스트 의도 표시

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,18 +7,18 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        //시작 출력
+        //시작 출력 및 사용자 입력 보관
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
         System.out.println("시도할 횟수는 몇 회인가요");
         String count = Console.readLine();
         OutView.printStartMessage();
 
-        // 입력(참가자, 시도 횟수)
+        //입력 검증(참가자, 시도 횟수)
         List<String> carNames = InputHandler.parseCarNames(names);
         int tryCount = InputHandler.getTryCount(count);
 
-        // 입력 결과 가공, 각 턴의 결과 출력
+        //입력 결과 가공, 각 턴의 결과 출력
         List<Car> cars = OutputHandler.createCars(carNames);
 
         for (int i = 0; i < tryCount; i++) {
--- src/test/java/racingcar/InputHandlerTest.java
+++ src/test/java/racingcar/InputHandlerTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.*;
 
 public class InputHandlerTest {
     @Test
+    @DisplayName("자동차 이름이 null Test")
     void validateNullName() {
         assertThatThrownBy(() -> InputHandler.validateCarName(null))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -15,6 +17,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("자동차 이름이 공백 space Test")
     void validateBlankName() {
         assertThatThrownBy(() -> InputHandler.validateCarName("   "))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -22,6 +25,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("자동차 이름이 공백 Test")
     void validateEmptyName() {
         assertThatThrownBy(() -> InputHandler.validateCarName(""))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -29,6 +33,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("자동차 이름 길이 준수 Test")
     void validateNameLength() {
         assertThatThrownBy(() -> InputHandler.validateCarName("abcdef"))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -36,6 +41,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("자동차 이름이 영문자 획인 Test")
     void validateAlphabetName() {
         assertThatThrownBy(() -> InputHandler.validateCarName("abc1"))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -43,6 +49,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("자동차 이름이 특수문자 예외 확인 Test")
     void validateCarNameContainsSymbol() {
         assertThatThrownBy(() -> InputHandler.validateCarName("car!"))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -50,6 +57,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("자동차 이름이 정해둔 모든 기준 요소를 통과하는지 Test")
     void validateCarNameNoException() {
         assertThatCode(() -> InputHandler.validateCarName("pobi"))
                 .doesNotThrowAnyException();
@@ -63,6 +71,7 @@ public class InputHandlerTest {
 
     // parseCarNames
     @Test
+    @DisplayName("정상적인 입력 문자열, 쉼표 기준으로 분리 후 리스트로 반환 Test")
     void parseCarNamesValidInput() {
         // given
         String input = "pobi,woni,jun";
@@ -77,6 +86,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("입력에 공백이 포함되어 있으면 trim 처리 후 유효한 이름으로 반환 Test")
     void parseCarNamesTestTrim() {
         // given
         String input = " pobi , woni , jun ";
@@ -90,12 +100,14 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("parseCarNames 입력이 null Test")
     void parseCarNamesNullInput() {
         assertThatThrownBy(() -> InputHandler.parseCarNames(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Input is empty or blank input");
     }
     @Test
+    @DisplayName("parseCarNames 입력이 공백 문자열 Test")
     void parseCarNamesBlankInput() {
         assertThatThrownBy(() -> InputHandler.parseCarNames("   "))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -104,6 +116,7 @@ public class InputHandlerTest {
 
     // getTryCount
     @Test
+    @DisplayName("시도 횟수 정상 입력 및 정수 변환 Test")
     void getTryCountValidInput() {
         // given
         String input = "5";
@@ -116,6 +129,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("시도 횟수 입력 0에 대한 정상 입력")
     void getTryCountZero() { // 이후 0일때는 따로 결과를 바로 표시하도록 진행
         // given
         String input = "0";
@@ -128,6 +142,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("시도 횟수 입력이 null Test")
     void getTryCountNullInput() {
         assertThatThrownBy(() -> InputHandler.getTryCount(null))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -135,6 +150,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("시도 횟수 입력이 공백 Test")
     void getTryCountBlankInput() {
         assertThatThrownBy(() -> InputHandler.getTryCount("   "))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -142,6 +158,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("시도 횟수 입력이 정수가 아닌 경우 Test")
     void getTryCountNonNumericInput() {
         assertThatThrownBy(() -> InputHandler.getTryCount("abc"))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -149,6 +166,7 @@ public class InputHandlerTest {
     }
 
     @Test
+    @DisplayName("시도 횟수 입력이 음수일 경우 Test")
     void getTryCountNegativeNumber() {
         assertThatThrownBy(() -> InputHandler.getTryCount("-3"))
                 .isInstanceOf(IllegalArgumentException.class)
