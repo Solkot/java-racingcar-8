ca69187f1f60869f918403bd83a758a587242883
Sole <chawoo9905@gmail.com>
2025-10-27 17:16:02 +0900

    feat(OutView): printRaceResult, 자동차 이동 결과 출력 기능 구현

---
+++ src/main/java/racingcar/OutView.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.List;
+
+public class OutView {
+    private static final String EXECUTION_RESULT_MESSAGE = "\n실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+
+    public static void printStartMessage() {
+        System.out.println(EXECUTION_RESULT_MESSAGE);
+    }
+
+    //OutputHandler의 getCurrentRaceState를 안 쓰는 이유는 책임 분리(단지 출력용), SRP 원칙을 지키기 위함
+    private static String formatCarProgress(Car car) {
+        return car.getName() + " : " + "-".repeat(car.getPosition());
+    }
+
+    public static void printRaceResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(formatCarProgress(car));
+        }
+        System.out.println();
+    }
+}
